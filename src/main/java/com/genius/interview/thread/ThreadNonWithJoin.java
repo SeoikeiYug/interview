979eb0b35e35df69bfd7734e7e559befebcfabeb
郁帅奇 <shuaiqi@kaixingame.cn>
2021-09-16 20:17:09 +0800

    Initial commit

---
+++ src/main/java/com/genius/interview/thread/ThreadNonWithJoin.java
@@ -0,0 +1,47 @@
+package com.genius.interview.thread;
+
+public class ThreadNonWithJoin {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + "主线程运行开始！");
+        Thread2 thread1 = new Thread2("A");
+        Thread2 thread2 = new Thread2("B");
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "主线程运行结束！");
+    }
+
+}
+
+class Thread2 extends Thread {
+
+    private String name;
+
+    public Thread2(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "线程运行开始！");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("子线程" + name + "运行: " + i);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + "线程运行结束！");
+    }
+}
---
+++ src/main/java/com/genius/interview/thread/ThreadWithJoin.java
@@ -0,0 +1,37 @@
+package com.genius.interview.thread;
+
+public class ThreadWithJoin {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + "主线程运行开始！");
+        Thread1 thread1 = new Thread1("A");
+        Thread1 thread2 = new Thread1("B");
+        thread1.start();
+        thread2.start();
+        System.out.println(Thread.currentThread().getName() + "主线程运行结束！");
+    }
+
+}
+
+class Thread1 extends Thread {
+
+    private String name;
+
+    public Thread1(String name) {
+        super(name);
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "线程运行开始！");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("子线程" + name + "运行: " + i);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + "线程运行结束！");
+    }
+}
